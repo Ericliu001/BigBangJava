02d208d10cb9959629715a6ecaa36bea539f5c99
ericliu <ericliu@Erics-MacBook-Pro.local>
2017-05-31 14:39:38 +1000

    updates some values in Mutex.java

--- src/test/java/com/ericliu/concurrent/Mutex.java
+++ src/test/java/com/ericliu/concurrent/Mutex.java
@@ -4,18 +4,26 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 /**
  * Created by ericliu on 30/5/17.
+ * here is a minimal Mutex class, that uses synchronization state 100 to mean unlocked,
+ * and one to mean locked. This class does not need the value arguments supported for synchronization methods,
+ * so uses zero, and otherwise ignores them.
  */
 public class Mutex {
 
-    static class Sync extends AbstractQueuedSynchronizer{
+    static class Sync extends AbstractQueuedSynchronizer {
+
+        public Sync() {
+            setState(100); // set the initial state, being unlocked.
+        }
+
         @Override
         protected boolean tryAcquire(int ignore) {
-            return compareAndSetState(0,1);
+            return compareAndSetState(100, 1);
         }
 
         @Override
         protected boolean tryRelease(int ignore) {
-            setState(0);
+            setState(100);
             return true;
         }
     }
